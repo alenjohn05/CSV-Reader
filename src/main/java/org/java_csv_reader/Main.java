52abab6f2044d4184c9860c1588dae1070e51e6a
alenjohn05 <alenjohnmj@gmail.com>
2023-12-05 10:00:00 +0530

    added increase price processing

--- src/main/java/org/java_csv_reader/Main.java
+++ src/main/java/org/java_csv_reader/Main.java
@@ -8,19 +8,24 @@ public class Main {
         CsvReaderBean csvBeanReader = new CsvReaderBean();
         String csvFileName = "Book.csv";
 
-        // Read all data from the original CSV file
-        csvBeanReader.readCSVFile(csvFileName);
-
-        // Add a new book
-        csvBeanReader.addBook("0596527756", "Java Generics and Collections", "Naftali n & Philip Wadler", "O'Reilly Media", "10/24/2006", 27);
-
-        // Get all books including the newly added one
-        List<Book> updatedBooks = csvBeanReader.getBooks();
-
-        // Write the updated list of books to a new CSV file
-        csvBeanReader.writeCSVFile("netbook.csv", updatedBooks);
-
-        // Perform partial reading
-        csvBeanReader.partialReader(csvFileName);
+//        // Read all data from the original CSV file
+//        csvBeanReader.readCSVFile(csvFileName);
+//
+//        // Add a new book
+//        csvBeanReader.addBook("0596527756", "Java Generics and Collections", "Naftali n & Philip Wadler", "O'Reilly Media", "10/24/2006", 27);
+//
+//        // Get all books including the newly added one
+//        List<Book> updatedBooks = csvBeanReader.getBooks();
+//
+//        // Write the updated list of books to a new CSV file
+//        csvBeanReader.writeCSVFile("netbook.csv", updatedBooks);
+//
+//        // Perform partial reading
+//        csvBeanReader.partialReader(csvFileName);
+//
+//        // Reading Specific Columns from the CSV
+//        csvBeanReader.readSpecificColumns(csvFileName);
+        // Reading Columns from the CSV and increase the price
+        csvBeanReader.readCSVFileAndIncreasePrice(csvFileName);
     }
 }
