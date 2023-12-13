756edfb61a44124b09582580fda32f191989c05e
alenjohn05 <alenjohnmj@gmail.com>
2023-12-13 10:30:00 +0530

    updated the file content and permissons setup

--- file_operation/src/main/java/org/example/FileOperations.java
+++ file_operation/src/main/java/org/example/FileOperations.java
@@ -1,12 +1,20 @@
 package org.example;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
+import java.text.SimpleDateFormat;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FileOperations {
+    public FileOperations() {
+    }
+
     public void fileRenameMoveExample(){
         Path sourceFile = Paths.get("tmp/newfilename.txt");
         Path destinationFile = Paths.get("users/newfile.txt");
@@ -102,5 +110,76 @@ public class FileOperations {
         }
     }
 
-    public 
+    public void directoryCheckExample(){
+        String filePath = "tmp"; // Replace with your directory path
+        Path path = Paths.get(filePath);
+        if (Files.exists(path) && Files.isDirectory(path)) {
+            System.out.println("File is a directory.");
+        } else {
+            System.out.println("File is not a directory or does not exist.");
+        }
+    }
+
+    public void lastModifiedDateExample(){
+        String filePath = "users/sample.pdf"; // Replace with your file path
+        Path path = Paths.get(filePath);
+        if (Files.exists(path)) {
+            try {
+                long lastModified = Files.getLastModifiedTime(path).toMillis();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String formattedDate = sdf.format(lastModified);
+                System.out.println("Last modified date: " + formattedDate);
+            } catch (Exception e) {
+                System.out.println("Error getting last modified date: " + e.getMessage());
+            }
+        } else {
+            System.out.println("File does not exist.");
+        }
+    }
+
+    public void fileFilterExample(){
+        String directoryPath = "users"; // Replace with your directory path
+        String fileExtension = ".txt"; // Replace with the file extension you want to filter
+        File directory = new File(directoryPath);
+
+        if (directory.exists() && directory.isDirectory()) {
+            FilenameFilter filter = (dir, name) -> name.endsWith(fileExtension);
+            File[] matchingFiles = directory.listFiles(filter);
+            if (matchingFiles != null && matchingFiles.length > 0) {
+                System.out.println("Files with extension '" + fileExtension + "':");
+                for (File file : matchingFiles) {
+                    System.out.println(file.getName());
+                }
+            } else {
+                System.out.println("No files found with extension '" + fileExtension + "'.");
+            }
+        } else {
+            System.out.println("Directory does not exist or is not a directory.");
+        }
+    }
+    public void filePermissionsExample(){
+        String filePath = "users/sample.pdf"; // Replace with your file path
+
+        Path file = Paths.get(filePath);
+
+        if (Files.exists(file)) {
+            // Define permissions
+            Set<PosixFilePermission> permissions = new HashSet<>();
+            permissions.add(PosixFilePermission.OWNER_READ);
+            permissions.add(PosixFilePermission.OWNER_WRITE);
+
+            // Convert permissions to FileAttribute
+            try {
+                Files.setPosixFilePermissions(file, permissions);
+                System.out.println("File permissions set successfully.");
+            } catch (UnsupportedOperationException e) {
+                System.out.println("Operation not supported on this platform: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Error setting file permissions: " + e.getMessage());
+            }
+        } else {
+            System.out.println("File does not exist.");
+        }
+    }
+
 }
