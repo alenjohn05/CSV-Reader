c0c07673725f821ef6f42548c5495f5112dd092e
alenjohn05 <alenjohnmj@gmail.com>
2023-12-14 10:30:00 +0530

    updated the main files

--- file_operation/src/main/java/org/example/Main.java
+++ file_operation/src/main/java/org/example/Main.java
@@ -5,7 +5,15 @@ public class Main {
     public static void main(String[] args) {
         new CreateNewFile().createNewFile();
         new CreateNewFile().allSeperators();
-        // Delete A File
-        new DeleteAFile().deleteAFile();
+
+        //File Operations
+        new FileOperations().fileRenameMoveExample();
+        new FileOperations().getSizeofTheFile();
+        new FileOperations().getFileExtension();
+        new FileOperations().isFileGivenExists();
+
+//        // Delete A File and Directory
+//        new DeleteAFile().deleteAFile();
+//        new DeleteAFile().deleteDirectoryRecurrive();
     }
 }
