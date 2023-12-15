f853d4ceeb5aac3a7a9cd5097fa58f1f4821f9db
alenjohn05 <alenjohnmj@gmail.com>
2023-12-15 10:30:00 +0530

    Added the delete operation

--- file_operation/src/main/java/org/example/DeleteAFile.java
+++ file_operation/src/main/java/org/example/DeleteAFile.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import java.io.File;
+
 public class DeleteAFile {
+    public void deleteAFile() {
+        String filePath = "tmp/file.txt";
+        File fileToDelete = new File(filePath);
+        try {
+            if (fileToDelete.exists()) {
+                boolean isDeleted = fileToDelete.delete(); // Deletes the file
+                if (isDeleted) {
+                    System.out.println("File deleted successfully.");
+                } else {
+                    System.out.println("Unable to delete the file.");
+                }
+            } else {
+                System.out.println("File does not exist at the specified path.");
+            }
+        } catch (SecurityException e) {
+            System.out.println("SecurityException occurred: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("An unexpected error occurred: " + e.getMessage());
+        }
+    }
 }
