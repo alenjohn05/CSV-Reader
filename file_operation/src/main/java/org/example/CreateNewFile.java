d9ce65e0b5a373aa484f3ee7c70233de103f2131
alenjohn05 <alenjohnmj@gmail.com>
2023-12-15 10:30:00 +0530

    Added all the seperators

--- file_operation/src/main/java/org/example/CreateNewFile.java
+++ file_operation/src/main/java/org/example/CreateNewFile.java
@@ -8,20 +8,19 @@ import java.nio.file.Path;
 public class CreateNewFile {
     public void createNewFile() {
         try {
-            // Creates a File.txt in tmp with Absolute path
-            String fileSeparator = FileSystems.getDefault().getSeparator();
-            // Absolute path
-            String absoluteFilePath = fileSeparator + "Users" + fileSeparator + "pankaj" + fileSeparator + "file.txt";
-            // Create directories if they don't exist
-            Path filePath = FileSystems.getDefault().getPath(absoluteFilePath);
-            System.out.println(filePath);
-            Files.createDirectories(filePath.getParent());
-            File file = new File(String.valueOf(filePath));
-            if (file.createNewFile()) {
-                System.out.println(filePath + " File Created");
-            } else {
-                System.out.println("File " + filePath + " already exists");
-            }
+//            // Creates a File.txt in tmp with Absolute path
+//            String fileSeparator = FileSystems.getDefault().getSeparator();
+//            // Absolute path
+//            String absoluteFilePath = fileSeparator + "Users" + fileSeparator + "pankaj" + fileSeparator + "file.txt";
+//            // Create directories if they don't exist
+//            Path filePath = FileSystems.getDefault().getPath(absoluteFilePath);
+//            Files.createDirectories(filePath.getParent());
+//            File file = new File(String.valueOf(filePath));
+//            if (file.createNewFile()) {
+//                System.out.println(filePath + " File Created");
+//            } else {
+//                System.out.println("File " + filePath + " already exists");
+//            }
 
 
 
@@ -30,7 +29,7 @@ public class CreateNewFile {
             // Create directories if they don't exist
             Files.createDirectories(relativePath.getParent());
             // Create the file
-            file = new File(String.valueOf(relativePath));
+            File file = new File(String.valueOf(relativePath));
             if (file.createNewFile()) {
                 System.out.println(relativePath + " File Created");
             } else {
@@ -40,4 +39,15 @@ public class CreateNewFile {
             System.out.println("Error occurred in file creation: " + e.getMessage());
         }
     }
+
+    // Delete a file
+
+
+    public void allSeperators(){
+        System.out.println("File.separator = "+File.separator);
+        System.out.println("File.separatorChar = "+File.separatorChar);
+        System.out.println("File.pathSeparator = "+File.pathSeparator);
+        System.out.println("File.pathSeparatorChar = "+File.pathSeparatorChar);
+    }
+
 }
